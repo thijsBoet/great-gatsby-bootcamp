a606a1cf00938d78e12a0648770a63377bb4f9bb
Matthijs Boet <m.boet@chello.nl>
2020-07-29 09:52:39 +0200

    got GRAPHql playground working

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const index = () => {
     <Layout>
       <main>
         <h1>Home</h1>
-        <h2>I'm Matthijs, a front-end developer living in Rotterdam</h2>
+        <h2>I'm Matthijs, a front-end developer living in beautiful Rotterdam.</h2>
         <p>
           Need a developer? <Link to="/contact">Contact me</Link>
         </p>
